a78038b2606bcfe6b7e242b9beec993dc4efef01
Marco Antonio <marco.oliveira92@hotmail.com>
2020-03-04 16:04:40 -0300

    integrando-com-zeit

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    'https://cors-anywhere.herokuapp.com/https://tf378xyae1.execute-api.sa-east-1.amazonaws.com/prod',
+  baseURL: 'https://tf378xyae1.execute-api.sa-east-1.amazonaws.com/prod',
   headers: { 'Access-Control-Allow-Origin': '*' },
 });
 
