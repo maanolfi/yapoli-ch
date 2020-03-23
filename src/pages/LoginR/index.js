a264ad01e7b4c510c7f7110a89d9d54973b9e423
Marco Antonio <marco.oliveira92@hotmail.com>
2020-03-23 10:39:10 -0300

    tela-login

--- src/pages/LoginR/index.js
+++ src/pages/LoginR/index.js
@@ -1,39 +1,31 @@
 import React from 'react';
 
 import { Container, Wrapper, Content, Video } from './styles';
-import video from '../../images/videologin.mp4'
-import iconlogin from '../../images/iconlogin.svg'
+import video from '../../images/videologin.mp4';
+import iconlogin from '../../images/iconlogin.svg';
 
 export default function LoginR() {
   return (
     <Container>
-    <Wrapper>
+      <Wrapper>
+        <h1>RINGS</h1>
+        <form>
+          <label htmlFor="name">Nome</label>
+          <input type="text" id="name" />
 
-      <h1>RINGS</h1>
-      <form>
-        <label htmlFor="name">Nome</label>
-        <input type="text" id='name' />
+          <label htmlFor="password">Senha</label>
+          <input type="password" id="password" />
 
-        <label htmlFor="password">Senha</label>
-        <input type="password" id='password' />
+          <button>Entrar / Cadastrar</button>
+        </form>
+        <img src={iconlogin} alt="Kiwi standing on oval" />
+      </Wrapper>
 
-        <button>Entrar</button>
-      </form>
-      <img src={iconlogin} alt="Kiwi standing on oval" />
-    </Wrapper>
-
-    <Content>
-
-    <video autoplay='true' muted loop>
-      <source src={video} type="video/mp4" />
-    </video>
-
-    <div>
-      <h1>Heading</h1>
-      <p>Lorem ipsum...</p>
-      <button id="myBtn" onclick="myFunction()">Pause</button>
-    </div>
-    </Content>
+      <Content>
+        <video autoPlay="true" muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+      </Content>
     </Container>
   );
 }
--- src/pages/LoginR/styles.js
+++ src/pages/LoginR/styles.js
@@ -2,17 +2,18 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: .4fr 1fr;
+  grid-template-columns: 0.4fr 1fr;
+  background-color: red;
+  padding: 2rem;
 `;
-
 
 export const Wrapper = styled.nav`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-color: #A7C8F2;
+  background-color: #a7c8f2;
   border-radius: 0 5% 5% 0;
 
   form {
@@ -26,27 +27,25 @@ export const Wrapper = styled.nav`
     width: 100px;
     height: 100px;
   }
-`
+`;
 
 export const Content = styled.section`
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
 
   flex-direction: column;
 
   video {
-    height: 100vh;
+    height: 80%;
   }
 
   div {
-  position: fixed;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  color: #f1f1f1;
-  width: 100%;
-  padding: 20px;
+    position: fixed;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: #f1f1f1;
+    width: 100%;
+    padding: 20px;
   }
-
-`
-
+`;
